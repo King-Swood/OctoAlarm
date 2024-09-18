fe1cd833603c227f8301fe255cd961ed4747fabc
Darren Pearce <41091584+King-Swood@users.noreply.github.com>
2024-09-19 09:00:39 +1000

    Finished startup pattern, untested in hardware.

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,52 @@
 #include "PatternProcessor.h"
 #include <Arduino.h>
 
-const unsigned int FreqStart = 300;
-const unsigned int FreqEnd = 600;
-const unsigned int PauseTimeUS = 3000;
+namespace {
+constexpr unsigned int FreqStart = 300;
+constexpr unsigned int FreqEnd = 600;
+constexpr unsigned int PauseTimeUS = 3000;
 
-tHeartbeat<13, 1000> heartbeat{};
-tLEDPulse<10, 2000> ledPulse{};
-tButton<11> button{};
+constexpr int HeartbeatLEDPin = 13;
+constexpr int ButtonLEDPin = 10;
+constexpr int ButtonPin = 11;
+constexpr int BeeperPin = 3;
 
-// TODO: On startup, flash the main button LED and beep twice, along with the
-// heartbeat LED. Then set the Button LED to 50% brightness, and flash the
-// heartbeat consistently.
-
-// Buzzer is dio 3.
-
-struct tStartupPattern {
-  bool value;
-  long unsigned timeMS;
-};
-
-const tStartupPattern StartupPattern[] = {
-    {true, 500},
-    {false, 500},
-    {true, 500},
-    {false, 500},
-};
+tHeartbeat<HeartbeatLEDPin, 1000> heartbeat{};
+tLEDPulse<ButtonLEDPin, 2000> ledPulse{};
+tButton<ButtonPin> button{};
 
 #define ArrayItemCount(a, item) (sizeof(a) / sizeof(item))
+
+void StartupPatternBlocking()
+{
+
+  struct tStartupPatternValue {
+    bool ledState;
+    int beeperFreq;
+  };
+  using tStartupPatternItem = tPatternItem<tStartupPatternValue>;
+
+  static const tStartupPatternItem StartupPattern[] = {
+      {{true, 500}, 500},   {{false, 1000}, 500}, {{true, 500}, 500},
+      {{false, 1000}, 500}, {{false, 0}, 0},
+  };
+
+  tPatternProcessor<tStartupPatternItem> startupProcessor(
+      StartupPattern, ArrayItemCount(StartupPattern, tStartupPatternItem));
+
+  while (!startupProcessor.IsFinished()) {
+    startupProcessor.Update([](const tStartupPatternValue &value) {
+      digitalWrite(ButtonLEDPin, value.ledState);
+      if (value.beeperFreq == 0) {
+        noTone(BeeperPin);
+      }
+      else {
+        tone(BeeperPin, value.beeperFreq);
+      }
+    });
+  }
+}
+} // namespace
 
 void setup()
 {
@@ -39,17 +58,9 @@ void setup()
   Serial.print("OctoAlarm Version ");
   Serial.println(Globals::Version);
 
-  tPatternProcessor<tStartupPattern> startupProcessor(
-      StartupPattern, ArrayItemCount(StartupPattern, tStartupPattern));
-
-  while (!startupProcessor.IsFinished()) {
-    startupProcessor.Update([](bool value) { digitalWrite(10, value); });
-  }
+  StartupPatternBlocking();
 
   ledPulse.SetConstantBrightness(50);
-
-  //   pinMode(3, OUTPUT);
-  //   tone(3, 1000);
 }
 
 unsigned int currentFreq = FreqStart;
