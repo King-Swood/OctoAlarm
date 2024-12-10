227a8439a128d182efce8e71565a0556b0265e58
Darren Pearce <41091584+King-Swood@users.noreply.github.com>
2024-12-10 21:27:11 +1100

    Started adding structure ready for new theme state.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ constexpr int HeartbeatLEDPin = 13;
 constexpr int ButtonLEDPin = 10;
 constexpr int ButtonPin = 11;
 constexpr int BeeperPin = 3;
+constexpr long unsigned ButtonHoldMS = 1000;
 
 tHeartbeat<HeartbeatLEDPin, 1000> heartbeat{};
 tLEDPulse<ButtonLEDPin, 2000> ledPulse{};
@@ -21,67 +22,106 @@ tSiren<BeeperPin, 3000> siren{};
 
 void StartupPatternBlocking()
 {
-  struct tStartupPatternValue {
-    bool ledState;
-    int beeperFreq;
-  };
-  using tStartupPatternItem = tPatternItem<tStartupPatternValue>;
+    struct tStartupPatternValue {
+        bool ledState;
+        int beeperFreq;
+    };
+    using tStartupPatternItem = tPatternItem<tStartupPatternValue>;
 
-  static const tStartupPatternItem StartupPattern[] = {
-      {{true, 1000}, 50},
-      {{false, 0}, 50},
-      {{true, 1000}, 50},
-      {{false, 0}, 500},
-  };
+    static const tStartupPatternItem StartupPattern[] = {
+        {{true, 1000}, 50},
+        {{false, 0}, 50},
+        {{true, 1000}, 50},
+        {{false, 0}, 500},
+    };
 
-  tPatternProcessor<tStartupPatternItem> startupProcessor(
-      StartupPattern, ArrayItemCount(StartupPattern, tStartupPatternItem));
+    tPatternProcessor<tStartupPatternItem> startupProcessor(
+        StartupPattern, ArrayItemCount(StartupPattern, tStartupPatternItem));
 
-  while (!startupProcessor.IsFinished()) {
-    startupProcessor.Update([](const tStartupPatternValue &value) {
-      digitalWrite(ButtonLEDPin, value.ledState);
-      if (value.beeperFreq == 0) {
-        noTone(BeeperPin);
-      }
-      else {
-        tone(BeeperPin, value.beeperFreq);
-      }
-    });
-  }
+    while (!startupProcessor.IsFinished()) {
+        startupProcessor.Update([](const tStartupPatternValue &value) {
+            digitalWrite(ButtonLEDPin, value.ledState);
+            if (value.beeperFreq == 0) {
+                noTone(BeeperPin);
+            }
+            else {
+                tone(BeeperPin, value.beeperFreq);
+            }
+        });
+    }
 }
 } // namespace
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.print("OctoAlarm Version ");
-  Serial.println(Globals::Version);
+    Serial.begin(115200);
+    Serial.print("OctoAlarm Version ");
+    Serial.println(Globals::Version);
 
-  StartupPatternBlocking();
+    StartupPatternBlocking();
 
-  ledPulse.SetConstantBrightness(50);
+    ledPulse.SetConstantBrightness(50);
 }
 
-bool running{};
-bool lastRunning{};
+enum class eState { Idle, Alarming, Theme, Size };
+
+eState state_{};
+eState lastState_{eState::Size};
 
 void loop()
 {
-  heartbeat.Update();
-  ledPulse.Update();
-  button.Update();
-  siren.Update();
+    heartbeat.Update();
+    ledPulse.Update();
+    button.Update();
+    siren.Update();
 
-  if (button.JustPressed()) {
-    if (!running) {
-      running = true;
-      ledPulse.Start();
-      siren.Start();
+    bool firstTime = state_ != lastState_;
+    lastState_ = state_;
+
+    switch (state_) {
+    case eState::Idle:
+        if (firstTime) {
+            ledPulse.Stop();
+            siren.Stop();
+        }
+        if (button.JustReleased()) {
+            state_ = eState::Alarming;
+        }
+        else if (button.IsHeld(ButtonHoldMS)) {
+            state_ = eState::Theme;
+        }
+        break;
+    case eState::Alarming:
+        if (firstTime) {
+            ledPulse.Start();
+            siren.Start();
+        }
+        if (button.JustReleased()) {
+            state_ = eState::Idle;
+        }
+        break;
+    case eState::Theme:
+        if (firstTime) {
+        }
+        // TODO: Need to ignore the first release, as this is from the hold...
+        if (button.JustReleased()) {
+            state_ = eState::Idle;
+        }
+        break;
+    case eState::Size:
+        break;
     }
-    else {
-      running = false;
-      ledPulse.Stop();
-      siren.Stop();
-    }
-  }
+
+    // if (button.JustPressed()) {
+    //     if (!running) {
+    //         running = true;
+    //         ledPulse.Start();
+    //         siren.Start();
+    //     }
+    //     else {
+    //         running = false;
+    //         ledPulse.Stop();
+    //         siren.Stop();
+    //     }
+    // }
 }
